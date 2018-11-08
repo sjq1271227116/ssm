7489c363a345abe1cf6e95ead911fddcf8259d3e
sjq1271227116 <1271227116@qq.com>
2018-11-08 17:49:06 +0800

    ssm框架

---
+++ src/main/java/com/shsxt/dao/AccountMapper.java
@@ -0,0 +1,17 @@
+package com.shsxt.dao;
+
+import com.shsxt.po.Account;
+
+public interface AccountMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Account record);
+
+    int insertSelective(Account record);
+
+    Account selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Account record);
+
+    int updateByPrimaryKey(Account record);
+}
